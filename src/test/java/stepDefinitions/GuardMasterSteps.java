d8cf6eb678f5a390f877732a642b84f0e6f74177
REMYA <ramya@Latticedubai.com>
2026-02-02 10:06:32 +0530

    commit code data config guard

---
+++ src/test/java/stepDefinitions/GuardMasterSteps.java
@@ -0,0 +1,129 @@
+package stepDefinitions;
+
+
+import io.cucumber.java.en.Then;
+import org.openqa.selenium.WebDriver;
+import pages.GuardMasterPage;
+import utils.DriverFactory;
+import utils.TestDataGenerator;
+import java.io.IOException;
+
+public class GuardMasterSteps {
+    public static String generatePatrolMode;
+    public static String generatePatrolShiftName;
+    private final WebDriver driver = DriverFactory.getDriver();
+    GuardMasterPage tmp = new GuardMasterPage(driver);
+    TestDataGenerator dataGen = new TestDataGenerator();
+
+    @Then("User waits {int} seconds")
+    public void user_waits_seconds(int seconds) {
+        try {
+            Thread.sleep(seconds * 1000L);
+            System.out.println("Waited for " + seconds + " seconds.");
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            System.out.println("Wait interrupted: " + e.getMessage());
+        }
+    }
+
+    @Then("User clicks on {string} in side menu")
+    public void user_click_on_DataConfig(String title) throws InterruptedException {
+        //String xpath="//*[@class='nav-link active' and @id='10']";
+        String xpath = "//a[contains(@class,'nav-link active') and @id='10']";
+        String id = "10";
+
+        tmp.clickOnDataConfiguration(id);
+    }
+
+    @Then("User clicks on the guard optional {string} tab")
+    public void user_click_on_GuardOptional(String title) throws InterruptedException {
+        //String xpath="//*[@class='nav-link active' and @id='10']";
+        String xpath = "//*[@id='tab-datasetup']/div[2]/div/ul[3]/li[3]/a";
+        tmp.clickOnGuardOptional(xpath);
+    }
+
+    @Then("User clicks on button patrol add {string}")
+    public void user_click_on_AddPatrolMode(String btnAddPatrolMode) throws InterruptedException {
+        tmp.clickOnAddPatrolMode(btnAddPatrolMode);
+    }
+
+    @Then("User fills up the {string} Patrol mode details")
+    public void user_fill_up_the_patrol_mode_details(String type) throws IOException {
+         generatePatrolMode = dataGen.generatePatrolMode();
+        tmp.enterPatrolMode(generatePatrolMode);
+    }
+
+    @Then("User clicks on Patrol Mode save button")
+    public void userClicksOnPatrolModeSaveButton() {
+        tmp.userClicksOnPatrolModeSaveButton();
+    }
+
+    @Then("User verify if the Patrol Mode is created")
+    public void user_verify_the_success_message_after_creating_patrolMode() {
+        tmp.verifyPatrolModecreation(generatePatrolMode);
+    }
+
+    @Then("User clicks on first Valid Patrol Mode to delete")
+    public void user_clicks_on_first_active_patrol_mode_to_delete() {
+        tmp.clickActiveModetoDelete();
+    }
+
+    @Then("User verify if the Mode is deleted")
+    public void user_Verify_first_active_patrol_mode_to_delete() {
+        tmp.verifyPatrolModeDelete(generatePatrolMode);
+    }
+
+    @Then("User clicks on active Patrol mode in the grid")
+    public void user_clicks_on_first_active_patrol_mode_to_edit() {
+        tmp.clickActiveModetoEdit();
+    }
+
+    @Then ("User clicks on Mode Export to Excel button")
+    public void user_clicks_on_mode_export_to_excel_button() throws InterruptedException{
+        String xpath = "//*[@id='btnExportToExcel']";
+        tmp.ModeclickExportToExcel(xpath);
+    }
+//guard shift
+    @Then ("User clicks on the {string} in side menu")
+    public void user_click_on_Guardshift(String title) throws InterruptedException {
+        String xpath = "//*[@id='ctl00_ContentPlaceHolder1_RadAjxPanel1']/div/div[1]/div/div/div[1]/div/div/div/a[2]";
+        tmp.clickOnGuardShift(xpath);
+    }
+    @Then("User clicks on button shifts add {string}")
+    public void GuardshiftAdd(String btnAddPatrolShift) throws InterruptedException {
+        tmp.GuardshiftAdd(btnAddPatrolShift);
+    }
+    @Then("User fills up the {string} Patrol Shift details")
+    public void user_fill_up_the_patrol_shift_details(String shift) throws IOException {
+        generatePatrolShiftName = dataGen.generatePatrolShiftName();
+        String generatePatrolShiftCode = dataGen.generatePatrolShiftCode();
+        tmp.generatePatrolShiftName(generatePatrolShiftName);
+        tmp.generatePatrolShiftCode(generatePatrolShiftCode);
+        tmp.generatePatrolStartTime("12.00 AM");
+        tmp.generatePatrolEndTime("5.00 PM");
+    }
+    @Then("User clicks on Patrol Shift save button")
+    public void userClicksOnPatrolShiftSaveButton() {
+        tmp.userClicksOnPatrolModeShiftSaveButton();
+    }
+    @Then("User verify if the Patrol Shift is created")
+    public void verifyPatrolShiftSaveButton() {
+        tmp.verifyPatrolShiftSaveButton(generatePatrolShiftName);
+    }
+    @Then("User clicks on active Patrol Shift in the grid")
+    public void user_clicks_on_first_active_patrol_shift_to_edit() {
+        tmp.clickActiveShifttoEdit();
+    }
+
+    @Then("User clicks on first Valid Patrol Shift to delete")
+    public void user_clicks_on_first_active_patrol_shift_to_delete() {
+        tmp.clickActiveShifttoDelete();
+    }
+
+    @Then("User verify if the Shift is deleted")
+    public void user_Verify_first_active_patrol_shift_to_delete() {
+        tmp.verifyPatrolShiftDelete(generatePatrolShiftName);
+    }
+}
+
+
--- src/test/java/utils/TestDataGenerator.java
+++ src/test/java/utils/TestDataGenerator.java
@@ -1,6 +1,7 @@
 package utils;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Random;
 import java.util.UUID;
@@ -97,5 +98,68 @@ public class TestDataGenerator {
 
     public String generateMaxNoParticipants() {
         return String.valueOf(random.nextInt(100) + 1); // 1 to 100 participants
+    }
+    //Guard Data Config
+    //Guard Mode  Generate a random Patrol Mode
+    public String generatePatrolMode() {
+        String[] mode = {"tstSeg way", "tstFoot", "tstCar", "tstMopped", "tstVan"};
+        return mode[random.nextInt(mode.length)] + " " + UUID.randomUUID().toString().substring(0, 4);
+    }
+    //Guard Shift Generate a random Patrol Shift Name
+    public String generatePatrolShiftName() {
+        String[] shift = {"tstMorShift", "tstEveShift", "tstNightShift", "tstHeavyShift", "tstShortShift"};
+        return shift[random.nextInt(shift.length)] + " " + UUID.randomUUID().toString().substring(0, 4);
+    }
+    //Guard Shift Generate a random Patrol Shift Code
+    public String generatePatrolShiftCode() {
+            return String.valueOf(random.nextInt(1000));
+        }
+    //Sales-Client type
+    public String generateClientType() {
+        String[] mode = {"tstClient1", "tstClient2", "tstClient3", "tstClient4", "tstClient5"};
+        return mode[random.nextInt(mode.length)] + " " + UUID.randomUUID().toString().substring(0, 4);
+    }
+    //Sales-Enquiry type
+    public String generateEnquiryType() {
+        String[] mode = {"tstEnqType1", "tstEnqType2", "tstEnqType3", "tstEnqType4", "tstEnqType5"};
+        return mode[random.nextInt(mode.length)] + " " + UUID.randomUUID().toString().substring(0, 4);
+    }
+    //Sales Enquiry Source
+    public String generateEnquirySource() {
+        String[] mode = {"tstEnqSource1", "tstEnqSource2", "tstEnqSource3", "tstEnqSource4", "tstEnqSource5"};
+        return mode[random.nextInt(mode.length)] + " " + UUID.randomUUID().toString().substring(0, 4);
+    }
+    //Sales T and C
+    public String generateEnquiryTandC() {
+        String[] mode = {"tstEnqTandC1", "tstEnqTandC2", "tstEnqTandC3", "tstEnqTandC4", "tstEnqTandC5"};
+        return mode[random.nextInt(mode.length)] + " " + UUID.randomUUID().toString().substring(0, 4);
+    }
+    //Sales Exclusions
+    public String generateEnquiryExclusions() {
+        String[] mode = {"tstEnqExclusion1", "tstEnqExclusion2", "tstEnqExclusion3", "tstEnqExclusion4", "tstEnqExclusion5"};
+        return mode[random.nextInt(mode.length)] + " " + UUID.randomUUID().toString().substring(0, 4);
+    }
+    //Sales Payment Terms
+    public String generateEnquiryPaymentTerms() {
+        String[] mode = {"tstEnqPaymentTerms1", "tstEnqPaymentTerms2", "tstEnqPaymentTerms3", "tstEnqPaymentTerms4", "tstEnqPaymentTerms5"};
+        return mode[random.nextInt(mode.length)] + " " + UUID.randomUUID().toString().substring(0, 4);
+    }
+    //Guard Route
+    public String generateRouteCode() {
+        String[] mode = {"tRC1", "tRC2", "tRC3", "tRC4", "tRC5"};
+        return mode[random.nextInt(mode.length)] + " " + UUID.randomUUID().toString().substring(0, 3);
+    }
+    public String generateRouteName() {
+        String[] mode = {"tstRouteName1", "tstRouteName2", "tstRouteName3", "tstRouteName4", "tstRouteName5"};
+        return mode[random.nextInt(mode.length)] + " " + UUID.randomUUID().toString().substring(0, 4);
+    }
+    public String generateRouteDesc() {
+        String[] mode = {"tstRouteDesc1", "tstRouteDesc2", "tstRouteDesc3", "tstRouteDesc4", "tstRouteDesc5"};
+        return mode[random.nextInt(mode.length)] + " " + UUID.randomUUID().toString().substring(0, 4);
+    }
+    //Guard Schedule Generate a random Patrol Schedule Name
+    public String generatePatrolScheduleName() {
+        String[] schedule = {"tstSchedule1", "tstSchedule2", "tstSchedule3", "tstSchedule4", "tstSchedule5"};
+        return schedule[random.nextInt(schedule.length)] + " " + UUID.randomUUID().toString().substring(0, 4);
     }
 }
