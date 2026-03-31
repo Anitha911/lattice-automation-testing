17b3411b579e9d5b14189230109559fcdacd4987
REMYA <ramya@Latticedubai.com>
2026-03-31 09:35:29 +0530

    commit code data config guard, sales,Inventory,RM

--- src/test/java/utils/HelperUtils.java
+++ src/test/java/utils/HelperUtils.java
@@ -1,14 +1,20 @@
 package utils;
 
+import org.openqa.selenium.*;
+import org.testng.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.testng.Assert;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
+import java.util.List;
+import java.util.Random;
 
 public class HelperUtils {
 
     private final WebDriver driver;
-
+    private WebDriverWait wait;
     public HelperUtils(WebDriver driver) {
         this.driver = driver;
     }
@@ -43,5 +49,25 @@ public class HelperUtils {
         WebElement errorElement = driver.findElement(errorLocator);
         String actualErrorMessage = errorElement.getText().trim();
         Assert.assertEquals(actualErrorMessage, expectedErrorMessage, "Inline error message mismatch");
+    }
+
+    public void clickRandomElement(By locator) {
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+
+        List<WebElement> elements = wait.until(ExpectedConditions
+                .visibilityOfAllElementsLocatedBy(locator));
+
+        Assert.assertFalse(elements.isEmpty(), "No visible elements found: " + locator);
+
+        WebElement element = elements.get(new Random().nextInt(elements.size()));
+
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element);
+
+        try {
+            element.click();
+        } catch (Exception e) {
+            ((JavascriptExecutor) driver).executeScript("arguments[0].click();", element);
+        }
     }
 }
