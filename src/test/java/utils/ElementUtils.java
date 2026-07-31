a594757bf0707665869e4c8f1927ebd030544983
Manohar <Manohar.Reddy@Latticegulf.com>
2026-07-31 12:38:32 +0530

    few more changes for Audit, Location, Property, Contract Mangement-Master Pages

--- src/test/java/utils/ElementUtils.java
+++ src/test/java/utils/ElementUtils.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.io.FileHandler;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.interactions.Actions;
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
@@ -90,6 +91,7 @@ public class ElementUtils {
             // loader may not appear, ignore
         }
     }
+
 
     public static void waitForDropdownLoading() {
         try {
