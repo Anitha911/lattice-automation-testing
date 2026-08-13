b751266fdc5cbc9db0651d9685a3eb2cde86d19f
Sankari <Sankari@Latticedubai.com>
2026-08-13 15:18:39 +0530

    Completed  - Essential Services

--- src/test/java/pages/AssetManagementMastersPage.java
+++ src/test/java/pages/AssetManagementMastersPage.java
@@ -1,11 +1,8 @@
 package pages;
-import com.microsoft.schemas.office.visio.x2012.main.TriggerType;
 import org.openqa.selenium.*;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.testng.Assert;
-
 import java.time.Duration;
 import java.util.*;
 
